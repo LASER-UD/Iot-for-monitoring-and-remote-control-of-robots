de5de0ad8da841fc04345e1b23e22f2d37b8f96f
jeisongarcia9713 <ndpastranzamora@gmail.com>
2019-08-09 09:35:03 -0500

    Se divide los mensajes

--- ws.js
+++ ws.js
@@ -2,8 +2,8 @@ var WebSocketServer = require('ws').Server,
 wss = new WebSocketServer({ port: 8000 })
 webSockets = new WebSocketServer({ noServer: true });//Objeto vacio
 //webSockets={};
-conexiones = [1, 1];//
-
+conexiones = [1, 1, 1];//
+//hola
 var des;
 
 wss.on('connection', function (ws, req) {
@@ -18,6 +18,7 @@ wss.on('connection', function (ws, req) {
 					console.log('Jordan el mecanico se conecto');
 					webSockets[1].send(JSON.stringify({'userFrom': 'Rita', 'type': 'JC' }));
 					webSockets[2].send(JSON.stringify({'userFrom': 'Rita', 'type': 'MC' }));
+    	    webSockets[3].send(JSON.stringify({'userFrom': 'Rita', 'type': 'MC' }));
 				}
 			} else {
 				ws.send('usuario ya conectado');
@@ -34,6 +35,16 @@ wss.on('connection', function (ws, req) {
 				webSockets[2].send(JSON.stringify({'userFrom': 'Rita', 'type': 'MC' }));
 			}
 			break;
+   	case '/jordan1':
+			webSockets[3] = ws;
+			conexiones[3] = 0;
+			console.log('Jordan1 Conectado');
+			if (conexiones[1] == 0) {//El mecanico esta presente
+				console.log('Mecanico jordan se conecto');
+				//webSockets[1].send(JSON.stringify({'userFrom': 'Rita', 'type': 'JC' }));
+				webSockets[3].send(JSON.stringify({'userFrom': 'Rita', 'type': 'MC' }));
+			}
+			break;
 		default:
 			console.log('usuario sin identificacion');
 			break;
@@ -44,6 +55,8 @@ wss.on('connection', function (ws, req) {
 			try {
 				var request = JSON.stringify({'userFrom':text_data_json['userFrom'],'message':text_data_json['message'],'type':text_data_json['type']});
 				webSockets[parseInt(text_data_json['userTo'])].send(request);
+				if(text_data_json['userTo']=='2'){
+				console.log(text_data_json['message']);}
 			}
 			catch (error) {
 				var request = JSON.stringify({'userFrom':'Rita','type': 'MD'});
@@ -63,12 +76,22 @@ wss.on('connection', function (ws, req) {
 				console.log('mecanico desconectado');
 				if (conexiones[2] == 0) {//Jordan esta presente
 					console.log('Jordan mecanico Desconectado');
-					webSockets[2].send(JSON.stringify({ 'userFrom': 'Rita', 'type': 'MD' }));
+					webSockets[3].send(JSON.stringify({ 'userFrom': 'Rita', 'type': 'MD' }));
+          webSockets[2].send(JSON.stringify({ 'userFrom': 'Rita', 'type': 'MD' }));
 				}
 				break;
 			case '/jordan':
 				delete webSockets[2]
 				conexiones[2] = 1;//habilita el puerto
+				console.log('Jordan desconectado');
+				if(conexiones[1]==0){//El mecanico esta presente
+					console.log('Mecanico jordan Desconectado');
+					webSockets[1].send(JSON.stringify({ 'userFrom': 'Rita', 'type':'JD'}));
+				}
+				break;
+      case '/jordan1':
+				delete webSockets[3]
+				conexiones[3] = 1;//habilita el puerto
 				console.log('Jordan desconectado');
 				if(conexiones[1]==0){//El mecanico esta presente
 					console.log('Mecanico jordan Desconectado');
